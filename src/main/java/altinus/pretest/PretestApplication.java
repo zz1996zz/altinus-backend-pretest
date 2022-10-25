264fba518347eb8b9071e977550708460eb94aae
JeongSu <zz1996zz@naver.com>
2022-10-25 15:21:36 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/altinus/pretest/PretestApplication.java
@@ -0,0 +1,13 @@
+package altinus.pretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PretestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PretestApplication.class, args);
+	}
+
+}
