18d217426fad5f69785578b4a76b2cfa120e60fd
JeongSu <zz1996zz@naver.com>
2022-10-26 15:30:00 +0900

    feat: BaseTimeEntity 생성
    테이블 내 데이터의 마지막 수정시간, 최초 생성시간을 알 수 있다.

--- src/main/java/altinus/pretest/PretestApplication.java
+++ src/main/java/altinus/pretest/PretestApplication.java
@@ -2,8 +2,10 @@ package altinus.pretest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PretestApplication {
 
 	public static void main(String[] args) {
