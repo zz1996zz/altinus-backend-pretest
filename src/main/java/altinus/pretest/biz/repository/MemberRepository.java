16ce95d3e830fa331eb963c54b996a4cbdc2e50b
JeongSu <zz1996zz@naver.com>
2022-10-27 00:06:30 +0900

    feat: 이미 회원가입된 회원인지 확인하는 메소드 추가

--- src/main/java/altinus/pretest/biz/repository/MemberRepository.java
+++ src/main/java/altinus/pretest/biz/repository/MemberRepository.java
@@ -4,4 +4,6 @@ import altinus.pretest.biz.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
 }
