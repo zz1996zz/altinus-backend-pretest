31e3a6b1985a06672d784f877bc6331a6d5899ab
JeongSu <zz1996zz@naver.com>
2022-10-27 17:04:13 +0900

    feat: 테스트용 빈 등록 파일 생성

---
+++ src/test/java/altinus/pretest/TestConfig.java
@@ -0,0 +1,25 @@
+package altinus.pretest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@TestConfiguration
+public class TestConfig {
+
+    @Value("${aes.secret}")
+    String secret;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AesBytesEncryptor aesBytesEncryptor() {
+        return new AesBytesEncryptor(secret, "70726574657374");
+    }
+}
