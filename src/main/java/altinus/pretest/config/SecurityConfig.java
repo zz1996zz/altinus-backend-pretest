9bc7c598144431d8877247827b816eb8e4ca9a51
JeongSu <zz1996zz@naver.com>
2022-10-27 23:31:21 +0900

    feat: jwt관련 기능 추가

--- src/main/java/altinus/pretest/config/SecurityConfig.java
+++ src/main/java/altinus/pretest/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package altinus.pretest.config;
 
+import altinus.pretest.biz.service.RedisService;
+import altinus.pretest.config.jwt.JwtAccessDeniedHandler;
+import altinus.pretest.config.jwt.JwtAuthenticationEntryPoint;
+import altinus.pretest.config.jwt.JwtSecurityConfig;
+import altinus.pretest.config.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,10 +15,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final RedisService redisService;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final CorsFilter corsFilter;
 
     @Value("${aes.secret}")
     String secret;
@@ -36,9 +48,17 @@ public class SecurityConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilter(corsFilter)
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .and()
                 .authorizeRequests()
-                .anyRequest()
-                .permitAll();
+                .antMatchers("/auth/login", "/members/signup")
+                .permitAll()
+                .anyRequest().authenticated();
+
+        http.apply(new JwtSecurityConfig(tokenProvider, redisService));
 
         return http.build();
     }
