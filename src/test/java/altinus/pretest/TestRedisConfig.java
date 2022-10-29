f17370cd6d109e51e9005a2283e7f1fa4ac1e745
JeongSu <zz1996zz@naver.com>
2022-10-29 17:31:39 +0900

    feat: 임베디드 레디스 생성

---
+++ src/test/java/altinus/pretest/TestRedisConfig.java
@@ -0,0 +1,28 @@
+package altinus.pretest;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@TestConfiguration
+public class TestRedisConfig {
+
+    private RedisServer redisServer;
+
+    public TestRedisConfig() {
+        redisServer = new RedisServer(6379);
+    }
+
+    @PostConstruct
+    public void startRedis() {
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        redisServer.stop();
+    }
+
+}
