a17abad5d8010000e8abbef1c8a1e4e842d2b33c
JeongSu <zz1996zz@naver.com>
2022-10-29 17:29:14 +0900

    feat: 에러 처리 추가

--- src/main/java/altinus/pretest/biz/exception/MemberErrorResult.java
+++ src/main/java/altinus/pretest/biz/exception/MemberErrorResult.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum MemberErrorResult {
 
-    MEMBER_EXIST(HttpStatus.BAD_GATEWAY, "회원이 이미 존재합니다.");
+    MEMBER_EXIST(HttpStatus.BAD_GATEWAY, "회원이 이미 존재합니다."),
+    MEMBER_NOT_EXIST(HttpStatus.BAD_GATEWAY, "회원이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
